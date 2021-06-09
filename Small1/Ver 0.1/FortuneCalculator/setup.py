5c2fc3220e9ee698bf56fb60919d16230186013e
MisileLab <misileminecord@gmail.com>
2021-06-09 18:58:09 +0900

    feat: Fortune Calculator for 1.17

---
+++ Small1/Ver 0.1/FortuneCalculator/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Fortune Calculator',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/misilelab/project-misilelaboratory',
+    license='MisileLab License V1.0',
+    author='MisileLab',
+    author_email='misileminecord@gmail.com',
+    description='Minecraft Fortune Calculator'
+)
