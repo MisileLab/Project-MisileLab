28074ef9e251c9edb219582f6b14837c0705e3f5
MisileLab <misileminecord@gmail.com>
2021-06-09 23:47:26 +0900

    edit package

--- Small1/Ver 0.1/FortuneCalculator/setup.py
+++ Small1/Ver 0.1/FortuneCalculator/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Fortune Calculator',
     version='0.0.3',
-    packages=['FortuneCalculator'],
+    packages=['Calculator'],
     url='https://github.com/MisileLab/Project-MisileLab/tree/main/Small1/Ver%200.1/FortuneCalculator',
     license='MisileLab License V1.0',
     author='MisileLab',
